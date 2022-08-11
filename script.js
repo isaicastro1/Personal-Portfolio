cbf51e532804b32b100236ce77688de18e4bd5ff
Isai Castro <code>
2022-08-10 20:51:15 -0700

    Added animation for nav bar on menu click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menu = document.getElementById('checkbox');
 let nav = document.getElementById('nav');
-let refs = document.getElementById('refs');
+let wrapper = document.getElementById('wrapper');
 let checkbox = document.getElementById('checkbox');
 
 let num = 1;
@@ -12,37 +12,16 @@ window.addEventListener('resize', updateNavBar);
 
 // Nav Bar Opening and closing 
 menu.addEventListener('click', () => {
-    if (num % 2 === 0) {
-        refs.style.display = 'none';
-        num++;
-        nav.classList.toggle('open');
-        console.log(num);
-        return;
+
+    wrapper.classList.toggle('open-nav');
+    setTimeout(() => {
+        wrapper.style.zIndex = 0;
+    }, 600)
+    wrapper.style.zIndex = -1;
     }
-    nav.classList.toggle('open');
-    refs.style.display = 'block';
-    num ++;
-    console.log(num);
-});
+);
 
 function updateNavBar ()  {
-
-    viewportWidth = window.innerWidth;
-
-    if (viewportWidth > 700) {
-        nav.classList.remove('open');
-        console.log('VW > 700',num);
-        refs.style.display = 'flex';
-        // console.log('more than 700px', 'checkbox:', checkbox.checked);
-        checkbox.checked = false;
-        return;
-    }
-    if (!checkbox.checked) {
-        nav.classList.remove('open');
-        // checkbox.checked = false;
-        refs.style.display = 'none';
-        console.log('VW < 700',num);
-    }
-    // console.log('less than 700px', 'checkbox:' , checkbox.checked);
+    // viewportWidth = window.innerWidth;
 };
 
