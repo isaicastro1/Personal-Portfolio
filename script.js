79f5824c1f745d7b6168599e249087597b0a6ebc
Isai Castro <isaicstr@gmail.com>
2022-08-13 17:20:33 -0700

    Added first page colors and fixed animation when opening nav bar

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let checkbox = document.getElementById('checkbox');
 let navWrapper = document.getElementById('nav');
 let myName = document.getElementById('my-name');
 let subtitle = document.getElementById('subtitle');
+let firstPage = document.getElementById('first-page');
 
 // NAV BAR TRANSITIONS
 
@@ -13,16 +14,35 @@ menu.addEventListener('click', () => {
     wrapper.classList.toggle('open-nav');
     wrapper.style.zIndex = -2;
 
+    let count = 0;
+    
+    if (wrapper.classList.contains('open-nav')) {
+        myName.classList.toggle('lower');
+        count++;
+    };
+
+    if (count % 2 === 0) {
+        setTimeout(() => {
+            myName.classList.remove('lower');
+        }, 500)
+        // myName.classList.remove('lower');
+    }
+
+
     wrapper.ontransitionend = () => {
         wrapper.style.zIndex = 2;
+        // myName.classList.remove('lower');
     }
     
+    // myName.classList.remove('lower');
     wrapper.style.zIndex = -2;
 
     }
 );
 
 // ENDS NAV BAR
+
+// FRONT-PAGE TYPING ANIMATION
 
 window.addEventListener('load', () => {
     setTimeout(() => {
